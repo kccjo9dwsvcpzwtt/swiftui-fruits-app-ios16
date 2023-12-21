858abcad05201bf083a4c406059fa9e72594d6f3
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2023-12-21 15:40:33 +0200

    Initial Commit

---
+++ swiftui-fruits-app-ios16/swiftui_fruits_app_ios16App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_fruits_app_ios16App.swift
+//  swiftui-fruits-app-ios16
+//
+//  Created by Stanislav Ponomarenko on 2023-12-21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_fruits_app_ios16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
