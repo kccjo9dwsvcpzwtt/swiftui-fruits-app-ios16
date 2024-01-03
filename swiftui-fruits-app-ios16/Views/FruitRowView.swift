49502f58da4e317cecd1dc61c1e526ffc3500db7
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2024-01-03 20:17:42 +0100

    Add RowView of data. Add List View to ContentView Screen.

---
+++ swiftui-fruits-app-ios16/Views/FruitRowView.swift
@@ -0,0 +1,47 @@
+//
+//  FruitRowView.swift
+//  swiftui-fruits-app-ios16
+//
+//  Created by Stanislav Ponomarenko on 2024-01-03.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+  
+  //MARK: - PROPERTIES
+  
+  var fruit: Fruit
+  
+  //MARK: - BODY
+  
+  var body: some View {
+    HStack {
+      Image(fruit.image)
+        .renderingMode(.original)
+        .resizable()
+        .scaledToFit()
+        .frame(width: 80, height: 80, alignment: .center)
+        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
+        .background(
+          LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+        )
+        .cornerRadius(8)
+      
+      VStack(alignment: .leading, spacing: 5) {
+        Text(fruit.title)
+          .font(.title2)
+          .fontWeight(.bold)
+        Text(fruit.headline)
+          .font(.caption)
+          .foregroundStyle(.secondary)
+      } //: VSTACK
+    } //: HSTACK
+  }
+}
+
+#Preview {
+  FruitRowView(fruit: fruitsData[0])
+    .previewLayout(.sizeThatFits)
+    .padding()
+}
